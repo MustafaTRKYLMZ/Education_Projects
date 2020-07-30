e1b784e211bc2215e04fd8023131277a8bdbdd05
Mustafa <cana66331@gmail.com>
2020-07-30 11:33:34 +0200

    part3 all of them exercises

---
+++ FullStackOpen2020/part3/phonebook_backend_/build/precache-manifest.72822b421be177e9a0daf0f4f9b786dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027951fe53439d24125d0cefffe86392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b2369adb89f4b635dfe",
+    "url": "/static/css/main.ba20d5c2.chunk.css"
+  },
+  {
+    "revision": "45d0e15feb43108da5a3",
+    "url": "/static/js/2.b01dda6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b01dda6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2369adb89f4b635dfe",
+    "url": "/static/js/main.2d9e1dd6.chunk.js"
+  },
+  {
+    "revision": "e1dd61f92d76ddf53966",
+    "url": "/static/js/runtime-main.cb97306b.js"
+  }
+]);
---
+++ FullStackOpen2020/part3/phonebook_backend_/index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const morgan=require('morgan')
+const Person = require('./models/person')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+const { response } = require('express')
+app.use(bodyParser.json())
+app.use(express.static('build'))
+
+morgan.token('jsonBody', (req, res) => JSON.stringify(req.body))
+
+const morgenLogger = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['jsonBody'](req, res)
+  ].join(' ')
+})
+app.use(morgenLogger)
+
+app.get('/', (req, res) => {
+  res.send('<h1>Phonebook backend server</h1><p>Use /api/persons for REST interface</p>')
+})
+
+app.get('/info',(req,res,next) => {
+  Person.countDocuments({})
+    .then(count => {
+      res.send(`<p>Phonebook has info for ${count} people.</p><p>${new Date()}</p>`)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons', (req, res,next) => {
+  Person.find({})
+    .then(persons =>{
+      res.json(persons)
+    })
+    .catch(error =>next(error))
+})
+
+app.post('/api/persons', (request, response,next) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name and/or number missing'
+    })
+  }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+app.delete('/api/persons/:id', (request, response,next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { runValidators: true, new: true, context: 'query' })
+    .then(updatedPerson => {  response.json(updatedPerson) })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
